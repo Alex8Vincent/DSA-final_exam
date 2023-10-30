ca5baa191f603b3271913f0b08aaa7c9501558c5
Alex Vincent <tmquang031104@gmail.com>
2023-10-31 00:23:28 +0700

    baitaplon

---
+++ bai18/mystack.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#ifndef _mystack.cpp
+#define _mystack.cpp
+#define ll long long
+#define MOD 10e9+7
+using namespace std;
+template
+class mystack{
+    private:
+
+};
+
+#endif
+int main(){}
