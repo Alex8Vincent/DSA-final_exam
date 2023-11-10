e322090e94935309bebd3fb8bbcf3e1e48f2e609
Alex Vincent <tmquang031104@gmail.com>
2023-11-10 18:13:22 +0700

    ssss

--- bai18/mystack.cpp
+++ bai18/mystack.cpp
@@ -4,9 +4,29 @@
 #define ll long long
 #define MOD 10e9+7
 using namespace std;
-template
+template <class T>
 class mystack{
     private:
+        T *buf;
+        int n, cap;
+    public:
+        mystack() {n = cap = 0; buf == NULL;}
+        ~mystack() {if(buf) delete[]buf;}
+        int size() {return n;}
+        bool empty() {return n == 0;}
+        void pop() {n--;}
+        T &top() {return buf[n-1];}
+        void push(t x){
+            if(n==cap){
+                cap = cap?cap*2:1;
+                T *tmp = new T[cap];
+                for(int i = 0; i < n; i++) tmp[i] = buf[i];
+                if(buf) delete[]buf;
+                buf = tmp;
+            }
+            buf[n++] =  x;
+        }
+
 
 };
 
---
+++ bai23/xaydungdanhba.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MOD 10e9+7
+using namespace std;
+
+class node{
+    public:
+        int elem;
+        node *child[26] = {};
+        node(){elem = 1;}
+};
+
+void update(node *&H, char *p){
+    if(!H) H = new node();
+    else H->elem++;
+    if(*p) update(H->child[*p - 'a'],p+1);
+}
+
+int get(node *H, char *p){
+    if(!H) return 0;
+    if(*p == 0) return H->elem;
+    return get(H->child[*p - 'a'],p+1);
+}
+
+int main(){
+    node *root = nullptr;
+    vector<int> a;
+    int n;
+    char q[10],x[1000];
+    cin >> n;
+    while(n--){
+        cin >> q >> x;
+        if(q[0] == 'a') update(root,x);
+        else{
+            a.push_back(get(root,x));
+        }
+    }
+    for(auto x : a){
+        cout << x << endl;
+    }
+}
